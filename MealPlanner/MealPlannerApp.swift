cfb2a27d2155818922f78a6d0bccae60e54e27b8
Zachary <zachofasunset@me.com>
2021-02-01 09:18:57 -0600

    Initial Commit

---
+++ MealPlanner/MealPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerApp.swift
+//  MealPlanner
+//
+//  Created by Zach Vandervelden on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MealPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
